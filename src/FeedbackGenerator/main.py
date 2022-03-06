b7e8a241f226c672cf083a8dbdb464f2be848e2e
listdo <S1910307072@students.fh-hagenberg.at>
2022-03-06 16:41:48 +0100

    added better handling of template and assignment files

--- src/FeedbackGenerator/main.py
+++ src/FeedbackGenerator/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import xlsxwriter
-import os
+import glob, os
 
 table_class = "rwd-table"
 
@@ -26,94 +26,101 @@ def get_next_element(f):
     return f.readline().split(":")[1].rstrip();
 
 def create_grading_file():
-    exercise = r"./templates/cgb_ex1.temp"
-    stud_file = r"./data/students_g1.csv"
 
-    exercise_file = open(exercise, "r");
-    students_file = open(stud_file, "r");
+    template_root = r"./templates"
+    data_root = r"./data"
 
-    tasks = pd.read_csv(filepath_or_buffer=exercise, skiprows=4, delimiter=";");
-    students = pd.read_csv(filepath_or_buffer=stud_file, skiprows=1);
+    templates = os.listdir(template_root)
+    student_files = os.listdir(data_root)
 
-    assignment = Assignment();
+    for exercise in templates:
+        for groups in student_files:
+            exercise_file = open(f'{template_root}/{exercise}', "r");
+            students_file = open(f'{data_root}/{groups}', "r");
 
-    assignment.author = get_next_element(exercise_file);
-    assignment.lecture = get_next_element(exercise_file);
-    assignment.excercise = get_next_element(exercise_file);
+            tasks = pd.read_csv(filepath_or_buffer=f'{template_root}/{exercise}', skiprows=4, delimiter=";");
+            students = pd.read_csv(filepath_or_buffer=f'{data_root}/{groups}', skiprows=1);
 
-    group = get_next_element(students_file);
-    f_name = f'{assignment.lecture}_{assignment.excercise}_G{group}';
+            assignment = Assignment();
 
-    isExist = os.path.exists(f'{f_name}')
+            assignment.author = get_next_element(exercise_file);
+            assignment.lecture = get_next_element(exercise_file);
+            assignment.excercise = get_next_element(exercise_file);
 
-    if not isExist:
-        # Create a new directory because it does not exist
-        os.makedirs(f'{f_name}')
+            group = get_next_element(students_file);
+            file_name = f"{assignment.lecture}_{assignment.excercise}_G{group}";
+            f_name = f'generated/';
 
-    workbook = xlsxwriter.Workbook(f'./{f_name}/{f_name}.xlsx')
-    bold = workbook.add_format({'bold': True})
+            isExist = os.path.exists(f'{f_name}')
 
-    for name in students["name"]:
-        worksheet = workbook.add_worksheet(name=name)
+            if not isExist:
+                # Create a new directory because it does not exist
+                os.makedirs(f'{f_name}')
 
-        worksheet.write(0, 0, f"Author: {assignment.author}");
-        worksheet.write(0, 1, f"Student: {name}");
-        worksheet.write(0, 2, f'Lecture: {assignment.lecture}');
-        worksheet.write(0, 3, f'Assignment: {assignment.excercise}');
+            workbook = xlsxwriter.Workbook(f'./{f_name}/{file_name}.xlsx')
+            bold = workbook.add_format({'bold': True})
+
+            for name in students["name"]:
+                worksheet = workbook.add_worksheet(name=name)
+
+                worksheet.write(0, 0, f"Author: {assignment.author}");
+                worksheet.write(0, 1, f"Student: {name}");
+                worksheet.write(0, 2, f'Lecture: {assignment.lecture}');
+                worksheet.write(0, 3, f'Assignment: {assignment.excercise}');
 
 
-        worksheet.write(1, 0, f'Tasks', bold);
-        worksheet.write(1, 1, f'Description', bold);
-        worksheet.write(1, 2, f'Possible Points', bold);
-        worksheet.write(1, 3, f'Reached Points', bold);
-        worksheet.write(1, 4, f'Comment', bold);
+                worksheet.write(1, 0, f'Tasks', bold);
+                worksheet.write(1, 1, f'Description', bold);
+                worksheet.write(1, 2, f'Possible Points', bold);
+                worksheet.write(1, 3, f'Reached Points', bold);
+                worksheet.write(1, 4, f'Comment', bold);
 
-        i = 2;
-        start = 2;
+                i = 2;
+                start = 2;
 
-        for (index, row) in tasks.iterrows():
-            worksheet.write(i, 0, row["Task"])
-            worksheet.write(i, 1, row["SubTask"])
-            worksheet.write(i, 2, row["Points"])
-            i += 1
+                for (index, row) in tasks.iterrows():
+                    worksheet.write(i, 0, row["Task"])
+                    worksheet.write(i, 1, row["SubTask"])
+                    worksheet.write(i, 2, row["Points"])
+                    i += 1
 
-        worksheet.write(i, 1, f'SUM', bold)
-        worksheet.write(i, 2, f'=SUM(C{start}:C{i})')
-        worksheet.write(i, 3, f'=SUM(D{start}:D{i})')
+                worksheet.write(i, 1, f'SUM', bold)
+                worksheet.write(i, 2, f'=SUM(C{start}:C{i})')
+                worksheet.write(i, 3, f'=SUM(D{start}:D{i})')
 
-        for column in tasks:
-            column_width = max(tasks[column].astype(str).map(len).max(), len(column))
-            col_idx = tasks.columns.get_loc(column)
-            worksheet.set_column(col_idx, col_idx + 10, column_width)
+                for column in tasks:
+                    column_width = max(tasks[column].astype(str).map(len).max(), len(column))
+                    col_idx = tasks.columns.get_loc(column)
+                    worksheet.set_column(col_idx, col_idx + 10, column_width)
 
-    workbook.close()
+            workbook.close()
 
 def create_solution_file():
-    fname = "CGB4_BB_EX1_G1"
+    fname = "CGB4_BB_EX2_G2"
+    split = fname.split("_")
 
-    filename = f"./{fname}/{fname}.xlsx"
-    stud_file = r"./data/students_g1.csv"
+    group_name = split[len(split) - 1].lower()
+
+    filename = f"./generated/{fname}.xlsx"
+    stud_file = f"./data/students_{group_name}.csv"
 
     students = pd.read_csv(filepath_or_buffer=stud_file, skiprows=1)
-    students_file = open(stud_file, "r")
 
-    group = get_next_element(students_file)
     isExist = os.path.exists(f'{fname}')
 
     if not isExist:
         # Create a new directory because it does not exist
-        os.makedirs(f'{fname}')
+        os.makedirs(f'./grading/{fname}')
 
     for name in students["name"]:
         df = pd.read_excel(filename, sheet_name=name, index_col=[0])
         df = df.replace(np.nan, '', regex=True)
-        df.to_html(f"./{fname}/{name}.html")
-
-        print(df)
+        df.to_html(f"./grading/{fname}/{name}.html")
 
         file = open(filename, 'r', encoding="utf8", errors='replace').read()
+        file.replace("<table ", "<table class=\"" + table_class + "\" ")
 
-        file = file.replace("<table ", "<table class=\"" + table_class + "\" ")
+create_grading_file()
 
-#create_grading_file()
-create_solution_file()
+
+#create_solution_file()
